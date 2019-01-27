06d3b1b17b2f7b7dc0043b4ffbdc32df0e66bffa
Mark De May <markd@frii.com>
2019-01-26 17:58:53 -0700

    Show popup when span is clicked.

--- replacer.js
+++ replacer.js
@@ -1,3 +1,23 @@
+const showPopup = () => {
+  console.log("Clicked!");
+  const popupDiv = document.createElement("div");
+  const style = "position: absolute; " +
+    "border: 4px solid black; " +
+    "padding: 20px; " +
+    "left: 100px; " +
+    "top: 100px; " +
+    "background-color: yellow;";
+
+  popupDiv.setAttribute("style", style);
+  popupDiv.onclick = (event) => {
+    console.log("popup clicked!");
+    console.log(event);
+    event.target.parentElement.removeChild(event.target);
+  }
+  popupDiv.appendChild(document.createTextNode("Hi"));
+  document.body.append(popupDiv);
+}
+
 let treeWalker = document.createTreeWalker(
   document.body,
   NodeFilter.SHOW_TEXT,
@@ -20,13 +40,15 @@ while (treeWalker.nextNode()) {
 
     // Replace the found word with a new element
     let innerSpan = document.createElement("span");
+    innerSpan.onclick = showPopup;
+    innerSpan.setAttribute("href", "#");
     innerSpan.setAttribute("style", "background-color: #e0e0e0; color: green; font-size: larger;");
     innerSpan.appendChild(document.createTextNode(results[2]));
     span.appendChild(innerSpan);
 
     // Add the text after the found word to the span
     span.appendChild(document.createTextNode(results[3]));
-    
+
     parent.replaceChild(span, treeWalker.currentNode);
     break;
   }
